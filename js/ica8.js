5b25ce7ecad8bb140bcb93c548119187225dbebc
anhe6096 <anhe6096@colorado.edu>
2022-10-11 20:34:08 -0600

    hi

---
+++ js/ica8.js
@@ -0,0 +1,16 @@
+var btn = document.getElementById('button');
+
+var para = document.getElementById('content');
+
+btn.addEventListener('click', function() {
+	para.style.fontWeight = '800';
+});
+
+var btn = document.getElementById('button2');
+
+var p2 = document.getElementById('content');
+
+btn.addEventListener('click', function() {
+	p2.style.fontWeight = '50';
+});
+
